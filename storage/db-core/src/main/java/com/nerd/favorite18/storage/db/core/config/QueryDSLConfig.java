1eceab8d5397814afbee3c334c19d16739bfa316
Jong1 <whddnjs822@gmail.com>
2024-06-23 11:17:14 +0900

    feat : QueryDSL에서 사용할 Bean 생성 및 EntityManager Javax를 받는게 아닌, Jakarta를 사용하도록 변경.

---
+++ storage/db-core/src/main/java/com/nerd/favorite18/storage/db/core/config/QueryDSLConfig.java
@@ -0,0 +1,20 @@
+package com.nerd.favorite18.storage.db.core.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QueryDSLConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
