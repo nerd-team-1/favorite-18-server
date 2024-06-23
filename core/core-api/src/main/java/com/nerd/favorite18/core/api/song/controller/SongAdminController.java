a74efdd9e7648dcf94cea682a508d0bbaa5fa01d
Jong1 <whddnjs822@gmail.com>
2024-06-23 22:52:10 +0900

    feat : admin-api의 RequestMapping 변경

--- core/core-api/src/main/java/com/nerd/favorite18/core/api/song/controller/SongAdminController.java
+++ core/core-api/src/main/java/com/nerd/favorite18/core/api/song/controller/SongAdminController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/admin/songs")
+@RequestMapping("/admin-api/v1/songs")
 public class SongAdminController {
     private final SongBusiness songBusiness;
 
