74d8a13fc281df547e9fba92df3b0beb3d624b0b
Jong1 <whddnjs822@gmail.com>
2024-06-25 10:48:25 +0900

    feat : 설정 변경
    - QueryDSLConfig에서 JPQLTemplates.DEFAULT속성을 추가안하는 경우 SpringBoot3에서 QueryDSL의 기존 기능들이 작동하지 않는 이슈로 추가.
    - 완성된 Page객체를 다시 Response시 직렬화 이슈가 발생 가능하다는 Spring권고에 따라 Api모듈에 @EnableSpringDataWebSupport 어노테이션 추가.

---
+++ core/core-api/src/main/java/com/nerd/favorite18/core/api/_common/config/SpringDataWebConfig.java
@@ -0,0 +1,11 @@
+package com.nerd.favorite18.core.api._common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class SpringDataWebConfig {
+
+}
