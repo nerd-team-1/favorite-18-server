dae5a4e6c40a053f6e1201ed7c4d184968c36aa2
jake <epikeran@gmail.com>
2024-06-26 15:35:38 +0900

    [#30] fix : 초기 유저 데이터 변경
    - local 초기 DB sub id 와 email 을 숨김처리 했습니다. 본인 이메일로 사용가능합니다.

--- core/core-api/src/main/java/com/nerd/favorite18/CoreApiApplication.java
+++ core/core-api/src/main/java/com/nerd/favorite18/CoreApiApplication.java
@@ -9,6 +9,7 @@ import com.nerd.favorite18.storage.db.core.qna.entity.Qna;
 import com.nerd.favorite18.storage.db.core.qna.repository.QnaRepository;
 import com.nerd.favorite18.storage.db.core.user.entity.User;
 import com.nerd.favorite18.storage.db.core.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,6 +21,14 @@ import org.springframework.transaction.annotation.Transactional;
 @ConfigurationPropertiesScan
 @SpringBootApplication
 public class CoreApiApplication {
+    @Value("${oauth2.admin.sub-id}")
+    String adminSubId;
+    @Value("${oauth2.admin.email}")
+    String adminEmail;
+    @Value("${oauth2.user.sub-id}")
+    String userSubId;
+    @Value("${oauth2.user.email}")
+    String userEmail;
 
     public static void main(String[] args) {
         SpringApplication.run(CoreApiApplication.class, args);
@@ -29,14 +38,15 @@ public class CoreApiApplication {
     @Profile("local")
     @Transactional
     public CommandLineRunner dataLoader(UserRepository userRepository, QnaRepository qnaRepository) {
+
         return args -> {
             // 초기 유저 데이터 추가
             User adminUser1 = User.builder()
-                    .subId("google_104727441863579907650")
-                    .email("epikeran@gmail.com")
-                    .name("박중언")
+                    .subId(adminSubId)
+                    .email(adminEmail)
+                    .name("관리자")
                     .gender(UserGender.MAN)
-                    .thumbnail("https://lh3.googleusercontent.com/a/ACg8ocLxQbFPRICseQzEjPz5xiYa4qyb8vzt6uYd3TmEXSsSYFmGPA=s96-c")
+                    .thumbnail("https://lh3.googleusercontent.com/")
                     .role(UserRole.ADMIN)
                     .status(UserStatus.ACTIVE)
                     .build();
@@ -44,11 +54,11 @@ public class CoreApiApplication {
             userRepository.save(adminUser1);
 
             User user1 = User.builder()
-                    .subId("google_101313027458036775900")
-                    .email("devjake929@gmail.com")
-                    .name("Jake Park")
+                    .subId(userSubId)
+                    .email(userEmail)
+                    .name("사용자")
                     .gender(UserGender.MAN)
-                    .thumbnail("https://lh3.googleusercontent.com/a/ACg8ocKDjcOE64GmRwovwXDRyJYiHLdG9Lon28CdmmE49NNF19uB=s96-c")
+                    .thumbnail("https://lh3.googleusercontent.com/")
                     .role(UserRole.USER)
                     .status(UserStatus.ACTIVE)
                     .build();
