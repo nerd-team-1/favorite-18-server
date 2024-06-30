4c0ed95af529cd218fd2b43608cc0483114188ad
Jong1 <whddnjs822@gmail.com>
2024-06-30 15:16:20 +0900

    fix : Swagger에서 CORS가 발생되는 이슈 수정

--- core/core-api/src/main/java/com/nerd/favorite18/core/api/_common/config/SwaggerConfig.java
+++ core/core-api/src/main/java/com/nerd/favorite18/core/api/_common/config/SwaggerConfig.java
@@ -2,18 +2,42 @@ package com.nerd.favorite18.core.api._common.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    @Profile({"local", "local-dev"})
+    public OpenAPI customOpenAPILocal() {
+        Server server = new Server();
+        server.setUrl("http://localhost:8080");
+        server.setDescription("Local");
+
         return new OpenAPI()
                 .info(new Info()
                         .title("Favorite 18 API")
                         .version("1.0")
-                        .description("This is API Document of Favorite 18"));
+                        .description("This is API Document of Favorite 18"))
+            .servers(List.of(server));
+    }
+
+    @Bean
+    @Profile({"dev", "staging", "live", "prod"})
+    public OpenAPI customOpenAPIExcludeLocal() {
+        Server server = new Server();
+        server.setUrl("https://favorite18.kr");
+        server.setDescription("DevServer");
+
+        return new OpenAPI()
+            .info(new Info()
+                .title("Favorite 18 API")
+                .version("1.0")
+                .description("This is API Document of Favorite 18"))
+            .servers(List.of(server));
     }
 }
