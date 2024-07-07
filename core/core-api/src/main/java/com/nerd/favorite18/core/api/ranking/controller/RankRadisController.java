3222e062a048d8152532004ef0cb56442bfeb41d
최서영 <83854617+BellaChooi@users.noreply.github.com>
2024-07-07 22:10:26 +0900

    수정사항 업로드3

--- core/core-api/src/main/java/com/nerd/favorite18/core/api/ranking/controller/RankRadisController.java
+++ core/core-api/src/main/java/com/nerd/favorite18/core/api/ranking/controller/RankRadisController.java
@@ -3,11 +3,14 @@ package com.nerd.favorite18.core.api.ranking.controller;
 import com.nerd.favorite18.core.api._common.support.response.ApiResponse;
 import com.nerd.favorite18.core.api.ranking.dto.RankRedisDto;
 import com.nerd.favorite18.core.api.ranking.service.RedisRankService;
+import com.nerd.favorite18.storage.db.core.song.entity.Song;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
