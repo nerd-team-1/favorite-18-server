08bfb7016c049ca01836919a690d513f9b0eed56
최서영 <83854617+BellaChooi@users.noreply.github.com>
2024-07-21 15:17:58 +0900

    수정사항 업로드3

--- core/core-api/src/main/java/com/nerd/favorite18/core/api/ranking/controller/RankRadisController.java
+++ core/core-api/src/main/java/com/nerd/favorite18/core/api/ranking/controller/RankRadisController.java
@@ -2,6 +2,7 @@ package com.nerd.favorite18.core.api.ranking.controller;
 
 import com.nerd.favorite18.core.api._common.support.response.ApiResponse;
 import com.nerd.favorite18.core.api.ranking.dto.RankRedisDto;
+import com.nerd.favorite18.core.api.ranking.dto.request.RankAddRequest;
 import com.nerd.favorite18.core.api.ranking.service.RedisRankService;
 import com.nerd.favorite18.storage.db.core.song.entity.Song;
 import lombok.Getter;
@@ -20,19 +21,22 @@ public class RankRadisController {
 
     private final RedisRankService redisRankService;
 
+    /** 1~100위 노래ID 조회  */
     @GetMapping()
     public List<RankRedisDto> getTop100Songs() {
         return redisRankService.getTop100Songs();
     }
 
-    @PostMapping
-    public ApiResponse<Void> setSongCount(@PathVariable Long songId){
-        redisRankService.setSongCount(songId);
-        return ApiResponse.success();
+    /** 노래ID로 랭킹리스트 조회 */
+    @GetMapping("/{songId}")
+    public Optional<Song> getSongById(@PathVariable Long songId) {
+        return redisRankService.getSongById(songId);
     }
 
-    @GetMapping("/song")
-    public Optional<Song> getSongById(Long songId){
-        return redisRankService.getSongById(songId);
+    /** 노래 상세정보 클릭 시 */
+    @PostMapping("/{songId}")
+    public ApiResponse<Void> clickSong(@PathVariable Long songId, @RequestBody RankAddRequest rankAddRequest){
+        redisRankService.clickSong(songId, rankAddRequest);
+        return ApiResponse.success();
     }
 }
--- core/core-batch/src/main/java/com/nerd/favorite18/BatchApplication.java
+++ core/core-batch/src/main/java/com/nerd/favorite18/BatchApplication.java
@@ -4,10 +4,10 @@ package com.nerd.favorite18;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
-@EnableScheduling
 @SpringBootApplication
 public class BatchApplication {
     public static void main(String[] args) {
