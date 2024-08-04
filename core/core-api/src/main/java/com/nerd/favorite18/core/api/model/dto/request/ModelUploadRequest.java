d11accb1cf34facd305b8cc895f6122508f787f1
jake <epikeran@gmail.com>
2024-08-05 00:24:44 +0900

    [#65] feat : 모바일용 녹음본 업로드 API 추가

--- core/core-api/src/main/java/com/nerd/favorite18/core/api/model/dto/request/ModelUploadRequest.java
+++ core/core-api/src/main/java/com/nerd/favorite18/core/api/model/dto/request/ModelUploadRequest.java
@@ -1,8 +1,10 @@
 package com.nerd.favorite18.core.api.model.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ModelUploadRequest {
     Long songId;
 }
